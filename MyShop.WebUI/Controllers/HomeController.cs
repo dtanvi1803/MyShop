ff955dc34ee649ddf2b862f53572123bf1c90b02
Tanvi Desai <dtanvi1803@yahoo.co.in>
2019-07-10 16:00:50 +0530

    Product Category Filtering

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,10 +20,24 @@ namespace MyShop.WebUI.Controllers
             productCategories = productCategoryContext;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string Category = null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            // List<Product> products = context.Collection().ToList();
+            List<Product> pro;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+            if (Category == null)
+            {
+               pro = context.Collection().ToList();
+            }
+            else
+            {
+                pro = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = pro;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
         public ActionResult Details(string Id) {
             Product product = context.Find(Id);
