766cbedcb564919be9c560c82ad23ecff5304098
Tanvi Desai <dtanvi1803@yahoo.co.in>
2019-07-24 12:56:53 +0530

    OrderManagement contd.

--- MyShop.Services/OrderService.cs
+++ MyShop.Services/OrderService.cs
@@ -33,5 +33,18 @@ namespace MyShop.Services
             orderContext.Insert(baseOrder);
             orderContext.Commit();
         }
+        public List<Order> GetOrderList()
+        {
+            return orderContext.Collection().ToList();
+        }
+        public Order GetOrder(string Id)
+        {
+            return orderContext.Find(Id);
+        }
+        public void UpdateOrder(Order updatedOrder)
+        {
+            orderContext.update(updatedOrder);
+            orderContext.Commit();
+        }
     }
 }
